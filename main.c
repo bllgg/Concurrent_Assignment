f08f8fc6394be4db55d9b73285600f7ad274863b
bllgg <gunathilakebllg@gmail.com>
2020-05-16 12:24:45 +0530

    Automated the proces to generate csv files

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
 #include "linked_list.h"
 
@@ -12,60 +13,73 @@
 void generate_report();
 
 int main(){
-    /*
-    node *head = NULL;
-
-    printf("Running in serial mode\n");
-    int res;
-
-    for (size_t i = 0; i < 10; i++){
-        res = Insert((i+1)*2, &head);
-        printf("result of insert %ld %d\n", i, res);
-    }
-    Print(head);
-
-    for (size_t i = 0; i < 8; i++){
-        res = Delete((i+1)*3, &head);
-        printf("result of delete %ld %d\n", (i+1)*3, res);
-    }
-    Print(head);
+    unsigned long serial_runtime, mut_t1_runtime, mut_t2_runtime, mut_t4_runtime, mut_t8_runtime, rw_t1_runtime,  rw_t2_runtime, rw_t4_runtime, rw_t8_runtime;
     
-    for (size_t i = 1; i <= 6; i++){
-        res = Member(i*2, head);
-        printf("result %ld %d\n", i*2, res);
+    /*===================
+    |       case 1      |
+    ====================*/
+    int case_num = 1;
+    FILE *fp;
+    fp = fopen("output/Case_1.csv","w+");
+    fprintf(fp,"Serial, mut_t1, mut_t2, mut_t4, mut_t8, rw_t1, rw_t2, rw_t4, rw_t8\n");
+    for (size_t i = 0; i < 100; i++)
+    {
+        serial_runtime = test_serial_run(case_num);
+        mut_t1_runtime = test_mutex_run(case_num,1);
+        mut_t2_runtime = test_mutex_run(case_num,2);
+        mut_t4_runtime = test_mutex_run(case_num,4);
+        mut_t8_runtime = test_mutex_run(case_num,8);
+        rw_t1_runtime = test_rw_lock_run(case_num,1);
+        rw_t2_runtime = test_rw_lock_run(case_num,2);
+        rw_t4_runtime = test_rw_lock_run(case_num,4);
+        rw_t8_runtime = test_rw_lock_run(case_num,8);
+        fprintf(fp,"%lu, %lu, %lu, %lu, %lu, %lu, %lu, %lu, %lu\n",serial_runtime, mut_t1_runtime, mut_t2_runtime, mut_t4_runtime, mut_t8_runtime, rw_t1_runtime,  rw_t2_runtime, rw_t4_runtime, rw_t8_runtime);
     }
+    fclose(fp);
 
-    destructor(head);
-    */
-    
-    unsigned long runtime;
-    
-    runtime = test_serial_run(1);
-    printf("Serial Runtime case 1 = %lu \n", runtime);
+    /*===================
+    |       case 2      |
+    ====================*/
+    case_num = 2;
+    fp = fopen("output/Case_2.csv","w+");
+    fprintf(fp,"Serial, mut_t1, mut_t2, mut_t4, mut_t8, rw_t1, rw_t2, rw_t4, rw_t8\n");
+    for (size_t i = 0; i < 100; i++)
+    {
+        serial_runtime = test_serial_run(case_num);
+        mut_t1_runtime = test_mutex_run(case_num,1);
+        mut_t2_runtime = test_mutex_run(case_num,2);
+        mut_t4_runtime = test_mutex_run(case_num,4);
+        mut_t8_runtime = test_mutex_run(case_num,8);
+        rw_t1_runtime = test_rw_lock_run(case_num,1);
+        rw_t2_runtime = test_rw_lock_run(case_num,2);
+        rw_t4_runtime = test_rw_lock_run(case_num,4);
+        rw_t8_runtime = test_rw_lock_run(case_num,8);
+        fprintf(fp,"%lu, %lu, %lu, %lu, %lu, %lu, %lu, %lu, %lu\n",serial_runtime, mut_t1_runtime, mut_t2_runtime, mut_t4_runtime, mut_t8_runtime, rw_t1_runtime,  rw_t2_runtime, rw_t4_runtime, rw_t8_runtime);
 
-    runtime = test_serial_run(2);
-    printf("Serial Runtime case 2 = %lu \n", runtime);
+    }
+    fclose(fp);
 
-    runtime = test_serial_run(3);
-    printf("Serial Runtime case 3 = %lu \n", runtime);
+    /*===================
+    |       case 3      |
+    ====================*/
+    case_num = 3;
+    fp = fopen("output/Case_3.csv","w+");
+    fprintf(fp,"Serial, mut_t1, mut_t2, mut_t4, mut_t8, rw_t1, rw_t2, rw_t4, rw_t8\n");
+    for (size_t i = 0; i < 100; i++)
+    {
+        serial_runtime = test_serial_run(case_num);
+        mut_t1_runtime = test_mutex_run(case_num,1);
+        mut_t2_runtime = test_mutex_run(case_num,2);
+        mut_t4_runtime = test_mutex_run(case_num,4);
+        mut_t8_runtime = test_mutex_run(case_num,8);
+        rw_t1_runtime = test_rw_lock_run(case_num,1);
+        rw_t2_runtime = test_rw_lock_run(case_num,2);
+        rw_t4_runtime = test_rw_lock_run(case_num,4);
+        rw_t8_runtime = test_rw_lock_run(case_num,8);
+        fprintf(fp,"%lu, %lu, %lu, %lu, %lu, %lu, %lu, %lu, %lu\n",serial_runtime, mut_t1_runtime, mut_t2_runtime, mut_t4_runtime, mut_t8_runtime, rw_t1_runtime,  rw_t2_runtime, rw_t4_runtime, rw_t8_runtime);
 
-    runtime = test_mutex_run(1,2);
-    printf("Mutex Runtime = %lu \n", runtime);
-
-    runtime = test_mutex_run(2,2);
-    printf("Mutex Runtime = %lu \n", runtime);
-
-    runtime = test_mutex_run(2,2);
-    printf("Mutex Runtime = %lu \n", runtime);
-
-
-    runtime = test_rw_lock_run(1,2);
-    printf("RW_lock Runtime = %lu \n", runtime);
-
-    runtime = test_rw_lock_run(2,2);
-    printf("RW_lock Runtime = %lu \n", runtime);
-
-    runtime = test_rw_lock_run(2,2);
-    printf("RW_lock Runtime = %lu \n", runtime);
+    }
+    fclose(fp);
+    printf("ran \n");
     return 0;
 }
